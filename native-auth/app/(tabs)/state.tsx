dab8733980cd8df15d8e8146f72df4edbb697aa2
mrauthentik <umokeuchenna2020@gmail.com>
2025-06-06 15:33:17 +0100

    feat: Implement task management functionality with add and delete capabilities

--- native-auth/app/(tabs)/state.tsx
+++ native-auth/app/(tabs)/state.tsx
@@ -1,2 +1,23 @@
 import { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, Button, FlatList, TouchableOpacity } from 'react-native';
+
+
+export default function State(){
+    const [task, setTask] = useState('');
+    const [tasks, setTasks] = useState<{ id: string; text: string }[]>([]);
+
+    const handleAddTask = () =>{
+        if(task.trim()){
+            setTasks([...tasks, {id: Date.now().toString(), text: task}]);
+            setTask('');
+        }
+    }
+
+    const handleDeleteTask = (id:string) => {
+        setTasks (tasks.filter((task)=> task.id !== id))
+    }
+
+    return (
+        
+    )
+}
