078509a82dcbeb059706e750042ce105dbd7efc9
mrauthentik <umokeuchenna2020@gmail.com>
2025-06-06 14:52:42 +0100

    feat: Add navigation setup and home screen component

--- native-auth/app/(tabs)/index.tsx
+++ native-auth/app/(tabs)/index.tsx
@@ -1,38 +1,18 @@
-import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {Text} from 'react-native';
+const Stack = createNativeStackNavigator();
 
-export default function App () {
-  const handlePress = () => {
-    alert('Button has been Pressed!')
-  };
-  const handleLongPress = () => {
-    alert('Button has been Long Pressed!')
-  };
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to the App!</Text>
-      <Button title="Press Me" onPress={handlePress} />
-      <Text style={styles.text}>This is a simple button example.</Text>
-      <Text style={styles.text}>You can customize it further.</Text>
-        <Text style={{color:'teal', fontSize:18}}> Long Press this Button below</Text>
-      <TouchableOpacity onLongPress={handleLongPress} style={{ marginTop: 20, padding: 10, backgroundColor: '#007BFF', borderRadius: 5 }}>
-        <Text style={{ color: 'white', fontSize: 16 }}>Custom Touchable Button</Text>
-      </TouchableOpacity>
-    </View>
-  )
+function HomeScreen() {
+  return <Text>Home Screen</Text>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    fontFamily: 'Arial'
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  }
-})
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
