0453e71ab70b6b2ef460fe7e69321620bd3a3982
mrauthentik <umokeuchenna2020@gmail.com>
2025-06-06 15:25:05 +0100

    feat: Enhance App component with button interactions and UI improvements

--- native-auth/app/(tabs)/index.tsx
+++ native-auth/app/(tabs)/index.tsx
@@ -1,18 +1,57 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {Text} from 'react-native';
-const Stack = createNativeStackNavigator();
+import { useState } from "react";
+import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-function HomeScreen() {
-  return <Text>Home Screen</Text>;
-}
+const Stack = createNativeStackNavigator()
 
-export default function App() {
+
+export default function App () {
+
+  const [count, setCount] = useState(0);
+  function HomeScreen () {
+    return <Text>Home Screen</Text>
+  }
+  const handlePress = () => {
+    alert('Button has been Pressed!')
+  };
+  const handleLongPress = () => {
+    alert('Button has been Long Pressed!')
+  };
+
+  const countHandler = () => {
+    setCount(count + 1);
+    alert(`Button pressed ${count + 1} times`);
+  }
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to the App!</Text>
+      <Button title="Press Me" onPress={handlePress} />
+      <Text style={styles.text}>This is a simple button example.</Text>
+      <Text style={styles.text}>You can customize it further.</Text>
+        <Text style={{color:'teal', fontSize:18}}> Long Press this Button below</Text>
+      <TouchableOpacity onLongPress={handleLongPress} style={{ marginTop: 20, padding: 10, backgroundColor: '#007BFF', borderRadius: 5 }}>
+        <Text style={{ color: 'white', fontSize: 16 }}>Custom Touchable Button</Text>
+      </TouchableOpacity>
+
+      <Button title={`Count: ${count}`} onPress={countHandler}></Button>
+    
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    fontFamily: 'Arial'
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  }
+})
